8c38cba676cde5cba79c970c8296af1d2ff4b15e
Karina Cid <memento_mori.06@hotmail.com>
2020-10-21 00:46:06 -0300

    Update Repository
    Project is finished and the repository is actualized from the origin

---
+++ src/two.js
@@ -0,0 +1,9 @@
+let objectToJson = () => {
+
+let dragon = { name: "Toothless", race: "Night Fury", island: "Berk" };
+let objJson = JSON.stringify(dragon);
+
+return objJson;
+};
+
+export default objectToJson;
